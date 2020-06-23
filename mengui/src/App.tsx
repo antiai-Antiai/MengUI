0c2eea7ee111351fa93b693f6f020a4729407e22
antiai <1603818113@qq.com>
2020-06-24 07:37:11 +0800

    menu

--- mengui/src/App.tsx
+++ mengui/src/App.tsx
@@ -1,17 +1,48 @@
 import React from 'react';
 import Button, { ButtonType, ButtonSize } from  './components/Button/button'
+import Menu from './components/Menu/menu'
+import MenuItem from './components/Menu/menuItem'
+import SubMenu from './components/Menu/subMenu'
 
 function App() {
   return (
-    <div className="App">
-      <Button onClick={(e) => {e.preventDefault();alert('ddd')}}> Default </Button>
-      <Button btnType={ButtonType.Primary}>Primary</Button>
-      <Button btnType={ButtonType.Danger}> Danger </Button>
-      <Button disabled> Hello </Button>
-      <Button size={ButtonSize.Small}>small</Button>
-      <Button size={ButtonSize.large}>Large</Button>
-      <Button btnType={ButtonType.Link} href={'http://www.baidu.com'} target="_blank"> Link </Button>
-      <Button btnType={ButtonType.Link} href={'http://www.baidu.com'} disabled> Disabled Link </Button>
+    <div>
+      {/* Button */}
+      <section className="Button">
+        <Button onClick={(e) => {e.preventDefault();alert('ddd')}}> Default </Button>
+        <Button btnType={ButtonType.Primary}>Primary</Button>
+        <Button btnType={ButtonType.Danger}> Danger </Button>
+        <Button disabled> Hello </Button>
+        <Button size={ButtonSize.Small}>small</Button>
+        <Button size={ButtonSize.large}>Large</Button>
+        <Button btnType={ButtonType.Link} href={'http://www.baidu.com'} target="_blank"> Link </Button>
+        <Button btnType={ButtonType.Link} href={'http://www.baidu.com'} disabled> Disabled Link </Button>
+      </section>
+      {/* Menu */}
+      <section className="Menu">
+        <Menu defaultIndex='0' 
+          onSelect={(index) => {alert(index)}} 
+          defaultOpenSubMenus={['2']}
+          mode={'vertical'}>
+          <MenuItem disabled>
+            cool link
+          </MenuItem>
+          <MenuItem>
+            cool link2
+          </MenuItem>
+          <SubMenu title="dropdown">
+            <MenuItem>
+              cool link3 1
+            </MenuItem>
+            <MenuItem>
+              cool link3 2
+            </MenuItem>
+          </SubMenu>
+          <MenuItem>
+            cool link3
+          </MenuItem>
+        </Menu>
+      </section>
     </div>
   );
 }
---
+++ mengui/src/components/Menu/menu.test.tsx
@@ -0,0 +1,101 @@
+import React from 'react'
+import { render, RenderResult, fireEvent, cleanup, wait } from '@testing-library/react'
+
+import Menu, { MenuProps } from './menu'
+import MenuItem from './menuItem'
+import SubMenu from './subMenu'
+
+const testProps:MenuProps = {
+    defaultIndex: '0',
+    onSelect: jest.fn(),
+    className:'test'
+}
+
+const testVerProps: MenuProps = {
+    defaultIndex: '0',
+    mode: 'vertical',
+}
+
+const generateMenu = (props: MenuProps) => {
+    return (
+      <Menu {...props}>
+        <MenuItem>
+          active
+        </MenuItem>
+        <MenuItem disabled>
+          disabled
+        </MenuItem>
+        <MenuItem>
+          xyz
+        </MenuItem>
+        <SubMenu title="dropdown">
+          <MenuItem>
+            drop1
+          </MenuItem>
+        </SubMenu>
+      </Menu>
+    )
+}
+
+const createStyleFile = () => {
+  const cssFile: string = `
+    .meng-submenu{
+      display: none;
+    }
+    .meng-submenu.menu-opened{
+      display:block;
+    }
+  `
+  const style = document.createElement('style')
+  style.type = 'text/css'
+  style.innerHTML = cssFile
+  return style
+}
+
+let wrapper:RenderResult,menuElement:HTMLElement,activeElement:HTMLElement,disabledElement:HTMLElement
+describe('test Menu and MenuItem componet', () => {
+    beforeEach(() => {
+        wrapper = render(generateMenu(testProps))
+        wrapper.container.append(createStyleFile())
+        menuElement= wrapper.getByTestId('test-menu')
+        activeElement = wrapper.getByText('active')
+        disabledElement = wrapper.getByText('disabled')
+    })
+    it('should render correct Menu and MenuItem based on default props',() => {
+        expect(menuElement).toBeInTheDocument()
+        expect(menuElement).toHaveClass('meng-menu test')
+        expect(menuElement.querySelectorAll(':scope > li').length).toEqual(4)
+        expect(activeElement).toHaveClass('menu-item is-active')
+        expect(disabledElement).toHaveClass('menu-item is-disabled')
+    })
+    it('click items should change active and call the right callback', () => {
+        const thridItem = wrapper.getByText('xyz')
+        fireEvent.click(thridItem)
+        expect(thridItem).toHaveClass('is-active')
+        expect(activeElement).not.toHaveClass('is-active')
+        expect(testProps.onSelect).toHaveBeenCalledWith('2')
+        fireEvent.click(disabledElement)
+        expect(disabledElement).not.toHaveClass('is-active')
+        expect(testProps.onSelect).not.toHaveBeenCalledWith(1)
+    })
+    it('should render vertical mode when mode is set to vertical', () => {
+        cleanup()
+        const wrapper = render(generateMenu(testVerProps))
+        const menuElement = wrapper.getByTestId('test-menu')
+        expect(menuElement).toHaveClass('meng-menu menu-vertical')
+    })
+    it('should show dropdown items when hover on subMenu', async() => {
+      expect(wrapper.queryByText('drop1')).not.toBeVisible()
+      const dropdownElement = wrapper.getByText('dropdown')
+      fireEvent.mouseEnter(dropdownElement)
+      await wait(() => {
+        expect(wrapper.queryByText('drop1')).toBeVisible()
+      })
+      fireEvent.click(wrapper.getByText('drop1'))
+      expect(testProps.onSelect).toHaveBeenLastCalledWith('3-0')
+      fireEvent.click(dropdownElement)
+      await wait(() => {
+        expect(wrapper.queryByText('drop1')).not.toBeVisible()
+      })
+    })
+})
