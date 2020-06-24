33ba6bc8c8466f8eb5378f15600bba06b5eed5da
antiai <1603818113@qq.com>
2020-06-24 17:32:55 +0800

    storybook

--- mengui/src/components/Button/button.tsx
+++ mengui/src/components/Button/button.tsx
@@ -6,17 +6,28 @@ export type ButtonType = 'primary' | 'default' | 'danger' | 'link'
 
 interface BaseButtonProps {
     className?: string;
-    disabled?:boolean;
-    size?:ButtonSize;
-    btnType?:ButtonType;
-    children: React.ReactNode,
-    href?:string
+    /**设置 Button 的禁用 */
+    disabled?: boolean;
+    /**设置 Button 的尺寸 */
+    size?: ButtonSize;
+    /**设置 Button 的类型 */
+    btnType?: ButtonType;
+    children: React.ReactNode;
+    href?: string;
 }
 
 type NativeButtonProps = BaseButtonProps & ButtonHTMLAttributes<HTMLElement>
 type AnchorButtonProps = BaseButtonProps & AnchorHTMLAttributes<HTMLElement>
 export type ButtonProps = Partial<NativeButtonProps & AnchorButtonProps>
-const Button:React.FC<ButtonProps> = (props) => {
+/**
+ * 页面中最常用的的按钮元素，适合于完成特定的交互
+ * ### 引用方法
+ * 
+ * ~~~js
+ * import { Button } from 'vikingship'
+ * ~~~
+ */
+export const Button:FC<ButtonProps> = (props) => {
     const {
         btnType,
         className,
@@ -60,4 +71,4 @@ Button.defaultProps = {
     btnType: 'default'
 }
 
-export default Button
+export default Button;
